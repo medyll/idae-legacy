8a325df39888fd2a293684f80dc4433e40e8293a
medyll <43032494+medyll@users.noreply.github.com>
2025-05-12 02:33:32 +0200

    Add cron job files for various intervals and templates
    - Created new cron job PHP files for minute, 5 minutes, 10 minutes, 15 minutes, and 30 seconds intervals in the blog, commercial, cruise, food delivery, customer, and other modules.
    - Added template cache files for app fiche mini and app fiche fields with structured HTML and PHP code.
    - Each cron job file contains a basic PHP opening tag for future implementation.

---
+++ web/tpl/app/cache/app-app_fiche-app_fiche_mini.latte--05c9f1bfa0.php
@@ -0,0 +1,38 @@
+<?php
+// source: D:\boulot\wamp64\www\idae.preprod.lan\web/bin/templates/app//app/app_fiche/app_fiche_mini.latte
+
+use Latte\Runtime as LR;
+
+class Template05c9f1bfa0 extends Latte\Runtime\Template
+{
+
+	function main()
+	{
+		extract($this->params);
+?>
+<div >
+	<div    onclick="save_setting_autoNext(this ,'<?php echo $table /* line 2 */ ?>_preview_fk')" style="border-bottom:1px solid <?php
+		echo $colorAppscheme /* line 2 */ ?>"  data-contextual="table=<?php echo LR\Filters::escapeHtmlAttr($table) /* line 2 */ ?>&table_value=<?php
+		echo LR\Filters::escapeHtmlAttr($table_value) /* line 2 */ ?>">
+		<div class="relative ">
+			<div class="flex_h flex_align_middle">
+				<div class="padding"><i class="fa fa-<?php echo $iconAppscheme /* line 5 */ ?>"></i></div>
+				<div class="padding ">
+					<div class="ellipsis"> <?php echo $nomAppscheme /* line 7 */ ?> </div>
+				</div>
+				<div class="padding flex_main">
+					<a class="ellipisis" act_chrome_gui="app/app/app_fiche"  vars="table=<?php echo LR\Filters::escapeHtmlAttr($table) /* line 10 */ ?>&table_value=<?php
+		echo LR\Filters::escapeHtmlAttr($table_value) /* line 10 */ ?>">le nom ci  </a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="relative  " style=" ">
+		<?php echo $tpl_table_fields /* line 16 */ ?>
+
+	</div>
+</div><?php
+		return get_defined_vars();
+	}
+
+}
